1958eb8670a88b50eaefe4009a19611101afd495
Andreea Megherlich <andreea.meghe@gmail.com>
2025-09-29 20:04:10 +0300

    turtle intro

---
+++ lectia4turtle.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.color("blue")
+turtle.shape("turtle")
+
+turtle.pendown()
+
+
+turtle.forward(30)
+turtle.left(90)
+
+turtle.forward(30)
+turtle.left(90)
+
+turtle.forward(30)
+turtle.left(90)
+
+turtle.forward(30)
+turtle.left(90)
+
+
+
+turtle.done()
