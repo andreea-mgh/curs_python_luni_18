0cdf7715df11160efad87b931d56b1efcbc32b89
Andreea Megherlich <andreea.meghe@gmail.com>
2025-10-06 19:35:47 +0300

    patrat si triunghi

--- lectia4turtle.py
+++ lectia4turtle.py
@@ -1,22 +1,32 @@
 import turtle
+# cum prescurtam numele functiilor din pachetele:
+from random import randint as rand
 
-turtle.color("blue")
+turtle.color("SkyBlue4")
 turtle.shape("turtle")
+turtle.speed(4)
 
 turtle.pendown()
 
+def patrat(latura=50):
+    for i in range(4): # 4 laturi
+        turtle.forward(latura)
+        turtle.left(90) # 360/4
 
-turtle.forward(30)
-turtle.left(90)
+def triunghi(latura):
+    for i in range(3): # 3 laturi
+        turtle.forward(latura)
+        turtle.left(120) # 360/3
 
-turtle.forward(30)
-turtle.left(90)
+# ca sa facem un poligon, ne rotim cu
+# 360 impartit la numarul de laturi
 
-turtle.forward(30)
-turtle.left(90)
 
-turtle.forward(30)
-turtle.left(90)
+for i in range(30): # repeta de 30 de ori
+    turtle.penup()
+    turtle.goto(rand(-150,150), rand(-100,100))
+    turtle.pendown()
+    triunghi(rand(20,80))
 
 
 
