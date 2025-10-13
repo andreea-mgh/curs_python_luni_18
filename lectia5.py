a780058961ccea6353c66f50838c21725911de85
Andreea Megherlich <andreea.meghe@gmail.com>
2025-10-13 19:11:46 +0300

    game map show map

--- lectia5.py
+++ lectia5.py
@@ -5,8 +5,26 @@ game_map = ['████████████████',
             '█$   ███  $  ███',
             '█    ! █     ! █',
             '█   ████     █ █',
-            '█   █    p   █ █',
+            '█   █    m   █ █',
             '█   !        █$█',
             '████████████████', ]
 
 game_map = [list(row) for row in game_map]
+
+# putem sa cautam caracterul de la o pozitie
+# cu game_map[linie][coloana]
+
+def show_map():
+    for linie in game_map:
+        print(''.join(linie))
+
+show_map()
+
+player_y = 6
+player_x = 9
+
+while True:
+    inst = input()
+    
+    if inst == 'up' and game_map[player_y-1][player_x] != '█':
+        player_y = player_y - 1
---
+++ test.py
@@ -0,0 +1,17 @@
+def mesaj(nume="Ana"):
+    print("Salut, " + nume)
+
+mesaj()
+mesaj("Ionela")
+
+
+def calculeaza(a, b):
+    return 2*a - b
+
+print( calculeaza( calculeaza(8,6), 12 ) )
+
+def calculeaza(a,b,c):
+    print(a*b - 3*c)
+
+calculeaza(3,6,3)
+
