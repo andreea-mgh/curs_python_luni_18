9ddbe592d732687a1346a5a9fab318bc676f2554
Andreea Megherlich <andreea.meghe@gmail.com>
2025-10-13 19:56:04 +0300

    collect gold

--- lectia5.py
+++ lectia5.py
@@ -18,13 +18,41 @@ def show_map():
     for linie in game_map:
         print(''.join(linie))
 
-show_map()
+HP = 10
+GOLD = 0
 
 player_y = 6
 player_x = 9
 
 while True:
+    show_map()
     inst = input()
-    
+
+    old_x = player_x
+    old_y = player_y
+
     if inst == 'up' and game_map[player_y-1][player_x] != '█':
         player_y = player_y - 1
+    
+    if inst == 'down' and game_map[player_y+1][player_x] != '█':
+        player_y = player_y + 1
+    
+    if inst == 'left' and game_map[player_y][player_x-1] != '█':
+        player_x = player_x - 1
+    
+    if inst == 'right' and game_map[player_y][player_x+1] != '█':
+        player_x = player_x + 1
+    
+    if inst == 'stat':
+        print(f"HP:   {HP}")
+        print(f"GOLD: {GOLD}")
+    
+    if inst in ['quit','exit','q','x']:
+        print("exiting game...")
+        break
+
+    if game_map[player_y][player_x] == '$':
+        GOLD += 1
+
+    game_map[old_y][old_x] = ' '
+    game_map[player_y][player_x] = 'm'
