b6766350ee083619b25fbcaa9f9041f5eef1ec93
Andreea Megherlich <andreea.meghe@gmail.com>
2025-10-20 19:26:49 +0300

    combat

--- lectia5.py
+++ lectia5.py
@@ -1,4 +1,6 @@
 import os
+from random import randint as rand
+from random import choice as choose
 
 def clear():
     os.system('cls')
@@ -8,7 +10,7 @@ game_map = ['████████████████',
             '██ $ █       ! █',
             '█$   ███  $  ███',
             '█    ! █     ! █',
-            '█   ████     █ █',
+            '█   ████   ███ █',
             '█   █    m   █ █',
             '█   !        █$█',
             '████████████████', ]
@@ -27,6 +29,33 @@ GOLD = 0
 
 player_y = 6
 player_x = 9
+
+def combat():
+    global HP
+    global GOLD
+    enemy_health = 4
+    print("Entering combat...")
+    while enemy_health > 0:
+        print(f"Enemy HP: {enemy_health}")
+        print(f"Your HP:  {HP}")
+        action = input()
+
+        if action in ['attack','a']:
+            enemy_health -= rand(1,2)
+
+            if enemy_health <= 0:
+                GOLD += rand(2,5)
+                print("Enemy defeated!")
+                return True
+            else:
+                HP -= 1
+                print(f"You were attacked, HP {HP}")
+
+                if HP <= 0:
+                    print("you died :((")
+                    return False
+
+
 
 while True:
     show_map()
@@ -58,6 +87,15 @@ while True:
 
     if game_map[player_y][player_x] == '$':
         GOLD += 1
+
+    if game_map[player_y][player_x] == '!':
+        fight = combat()
+        if fight:
+            print(f"combat ended! HP: {HP}, GOLD: {GOLD}")
+        else:
+            break
+    
+
 
     game_map[old_y][old_x] = ' '
     game_map[player_y][player_x] = 'm'
