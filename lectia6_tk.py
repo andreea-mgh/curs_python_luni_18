f6ef41fa5d3d5e9c4cf54ba3b27223186677d076
Andreea Megherlich <andreea.meghe@gmail.com>
2025-10-27 18:41:47 +0200

    tk intro

---
+++ lectia6_tk.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+win = tk.Tk()
+button = tk.Button(win, text="click me")
+button.pack()
+
+win.mainloop()
+
+
+# Toate functiile au paranteze dupa nume.
+
+# Cand cream un obiect, si acolo punem paranteze
+# dupa numele obiectului
+
+# Rolul parantezelor e sa transmitem informatii catre
+# chestia dupa care punem paranteze.
+#    ex. la functii transmitem parametri
+#        la obiecte transmitem trasaturile obiectelor
+
+# functiile sunt cu litere mici
+# obiectele cu litere mari
