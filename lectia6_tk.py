cd46d23c0aa8690c3ddcd434105f03024254ace5
Andreea Megherlich <andreea.meghe@gmail.com>
2025-10-27 19:17:35 +0200

    canvas+button

--- lectia6_tk.py
+++ lectia6_tk.py
@@ -1,7 +1,15 @@
 import tkinter as tk
+from random import randint as rand
 
 def chestie():
     print("ai apasat butonul")
+
+def add_shape():
+    x = rand(0,400)
+    y = rand(0,400)
+    w = rand(20,50)
+    h = rand(20,50)
+    canvas.create_oval(x, y, x+w, y+h, fill="white", outline="")
 
 # facem un obiect fereastra, il numim "win"
 win = tk.Tk()
@@ -12,6 +20,13 @@ win = tk.Tk()
 button = tk.Button(win, text="click me", command=chestie)
 # afiseaza butonul pe fereastra
 button.pack()
+
+buton2 = tk.Button(win, text="adauga obiect", command=add_shape)
+buton2.pack()
+
+# https://www.plus2net.com/python/tkinter-colors.php
+canvas = tk.Canvas(win, width=400, height=400, bg="lightblue")
+canvas.pack()
 
 win.mainloop()
 
