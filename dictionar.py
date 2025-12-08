dce7fc33b4843cbc989bfe54167e2fa8c798363d
Andreea Megherlich <andreea.meghe@gmail.com>
2025-12-08 19:11:16 +0200

    dict in python

---
+++ dictionar.py
@@ -0,0 +1,15 @@
+
+# un dictionar este o lista in care fiecare element
+# are propriul lui nume
+
+# numele elementului se cheama "cheie"
+# elementul in sine se cheama "valoare"
+
+chestii_despre_mine = {
+    "nume":"andreea",
+    "imi place":"jocuri si robotei",
+    "animal preferat":"papagal",
+    "varsta":23
+}
+
+print(chestii_despre_mine["imi place"])
